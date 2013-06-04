61fdd91315425b33b75ff6fbfdd52b86a8fee409
dzenanr <dzenanr@gmail.com>
2013-06-03 20:13:39 -0400

    uncomment observables for category

--- web/component/category/category_edit.dart
+++ web/component/category/category_edit.dart
@@ -5,13 +5,13 @@ import 'package:web_ui/web_ui.dart';
 
 class CategoryEdit extends WebComponent {
   Categories categories;
-  //@observable // does not help to see a new description in the list
+  @observable // does not help to see a new description in the list
   Category category;
 
   update() {
     InputElement description = query("#edit-category-description");
     category.description = description.value;
-    categories.order(); // to see a new description in the list
+    //categories.order(); // to see a new description in the list
     var categoryTable = query('#category-table').xtag;
     categoryTable.showCategoryEdit = false;
   }
